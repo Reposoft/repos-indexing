9c756be1c90c2ea9bbc6054ad478745e2a176fcf
Staffan Olsson <staffan@repos.se>
2013-10-17 11:10:08 +0000

    For logging from handler chain it is very helpful to get the core name, for example at commit

---
+++ src/main/java/se/repos/indexing/solrj/HttpSolrServerNamed.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2004-2012 Repos Mjukvara AB
+ */
+package se.repos.indexing.solrj;
+
+import org.apache.http.client.HttpClient;
+import org.apache.solr.client.solrj.ResponseParser;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+
+/**
+ * Default toString for HttpSolrServer is unhelpul in logs.
+ * We often deal with multiple cores and share some code between them, so it is useful to log core name/identifier.
+ */
+public class HttpSolrServerNamed extends HttpSolrServer {
+
+	public HttpSolrServerNamed(String baseURL) {
+		super(baseURL);
+	}
+
+	public HttpSolrServerNamed(String baseURL, HttpClient client) {
+		super(baseURL, client);
+	}
+	
+	public HttpSolrServerNamed(String arg0, HttpClient arg1, ResponseParser arg2) {
+		super(arg0, arg1, arg2);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private String identifier ;
+
+	public HttpSolrServerNamed setName(String identifier) {
+		this.identifier = identifier;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return super.getClass().getName() + "(" + identifier + ")";
+	}
+	
+}
