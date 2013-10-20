f36f38ba696cbd85afee27f6ed512b166d2eafdb
Staffan Olsson <staffan@repos.se>
2013-10-20 04:32:21 +0000

    Release repos-indexing 0.7:  * Adds IndexAdmin service to clear index per repository.  * Helper base class to configure IndexAdmin notification (typically clearing of other cores).  * Iteration of IndexingUnit, i.e. handlers and items, abstracted and based on a decision impl. The "blocking" scheduler is still our only one, best suited for testing.  * With this new handling, Markers can be both "trigger"ed and "ignore"d.  * "id" field is now urlencoded.  * "idhead" field added, useful for "refid".  * Handler chain can now be customized in more detail, but keeping a full list of handlers for specific purposes looks like a more readable option by now.

---
+++ src/main/java/se/repos/indexing/IndexAdminNotification.java
@@ -0,0 +1,28 @@
+package se.repos.indexing;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Configuration of index admins is a bit bulky so it will help future refactorings if notification receivers extends this class.
+ */
+@Singleton
+public abstract class IndexAdminNotification implements IndexAdmin {
+
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Inject
+	public final void setIndexAdminCentral(IndexAdmin cetnralIndexAdmin) {
+		cetnralIndexAdmin.addPostAction(this);
+		logger.info("Activated index admin {}", this);		
+	}
+	
+	@Override
+	public void addPostAction(IndexAdmin notificationReceiver) {
+		throw new UnsupportedOperationException("Not supported for notification receivers");
+	}
+
+}
