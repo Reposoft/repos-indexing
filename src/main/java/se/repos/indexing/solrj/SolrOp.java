2ac3c4e649530d1f04c3c1e7269813f26cb8fc0c
Thomas Åkesson <thomas.akesson@simonsoft.se>
2016-07-01 11:48:04 +0000

    Introduce sleep and retry when Solrj communication fails.

--- src/main/java/se/repos/indexing/solrj/SolrOp.java
+++ src/main/java/se/repos/indexing/solrj/SolrOp.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Shared error handling for solr operations.
@@ -14,6 +16,8 @@ import org.apache.solr.client.solrj.SolrServerException;
 public abstract class SolrOp implements Runnable {
 
 	protected SolrServer core;
+	public final Logger logger = LoggerFactory.getLogger(this.getClass());
+	public final Long retryPause = 10000L;
 
 	public SolrOp(SolrServer core) {
 		this.core = core;
@@ -24,11 +28,37 @@ public abstract class SolrOp implements Runnable {
 		try {
 			runOp();
 		} catch (SolrServerException e) {
-			throw new RuntimeException("Solr error not handled", e);
+			// Retry here as well, seems like IOExceptions sometimes are wrapped.
+			logger.warn("Solr first attempt failed with SolrServerException, retry in {} ms: {}", retryPause, e.getMessage());
+			logger.debug("Solr first attempt failed with SolrServerException: ", e);
+			retry();
 		} catch (IOException e) {
-			// retry?
-			throw new RuntimeException("Solr error not handled", e);
+			logger.warn("Solr first attempt failed with IOException, retry in {} ms: {}", retryPause, e.getMessage());
+			logger.debug("Solr first attempt failed with IOException: ", e);
+			retry();
 		}
+	}
+	
+	private void retry() {
+
+		try {
+			Thread.sleep(retryPause);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Retry sleep interrupted: " +  e.getMessage());
+		}
+		
+		logger.info("Solr second attempt...");
+		try {
+			runOp();
+		} catch (SolrServerException e) {
+			logger.warn("Solr second attempt failed with SolrServerException: ", e);
+			throw new RuntimeException("Solr exception during retry", e);
+		} catch (IOException e) {
+			logger.warn("Solr second attempt failed with IOException: ", e);
+			throw new RuntimeException("Solr exception during retry", e);
+		}
+		logger.info("Solr second attempt successful");
+		
 	}
 	
 	protected abstract void runOp() throws SolrServerException, IOException;
