6f125e2f88fb09d01c1338712fd21a78ae097ab8
Thomas Åkesson <thomas.akesson@simonsoft.se>
2020-12-02 11:48:58 +0100

    Handle RemoteSolrException which represents server-side SolR errors. Extends RuntimeException so it was previously not logged nor retried.

--- src/main/java/se/repos/indexing/solrj/SolrOp.java
+++ src/main/java/se/repos/indexing/solrj/SolrOp.java
@@ -5,8 +5,10 @@ package se.repos.indexing.solrj;
 
 import java.io.IOException;
 
+import org.apache.http.HttpStatus;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient.RemoteSolrException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,12 +29,21 @@ public abstract class SolrOp implements Runnable {
 	public void run() {
 		try {
 			runOp();
+		} catch (RemoteSolrException e) {
+			// #1358 Thrown when response != 200, unexpected content type, invalid response body.
+			// #1358 This extends RuntimeException (via SolrException) so it was never handled here before.
+			logger.warn("Solr first attempt failed with RemoteSolrException HTTP {}, retry in {} ms: {}", e.code(), retryPause, e.getMessage());
+			logger.debug("Solr first attempt failed with RemoteSolrException HTTP {}: ", e.code(), e);
+			retry();
 		} catch (SolrServerException e) {
 			// Retry here as well, seems like IOExceptions sometimes are wrapped.
+			// #1358 SolR 6.6.6 and SolR 8 seems to always wrap IOException in SolrServerException.
+			// See: https://github.com/apache/lucene-solr/blame/branch_6_6/solr/solrj/src/java/org/apache/solr/client/solrj/impl/HttpSolrClient.java
 			logger.warn("Solr first attempt failed with SolrServerException, retry in {} ms: {}", retryPause, e.getMessage());
 			logger.debug("Solr first attempt failed with SolrServerException: ", e);
 			retry();
 		} catch (IOException e) {
+			// #1358 Likely never happens.
 			logger.warn("Solr first attempt failed with IOException, retry in {} ms: {}", retryPause, e.getMessage());
 			logger.debug("Solr first attempt failed with IOException: ", e);
 			retry();
@@ -50,10 +61,14 @@ public abstract class SolrOp implements Runnable {
 		logger.info("Solr second attempt...");
 		try {
 			runOp();
+		} catch (RemoteSolrException e) {
+			logger.warn("Solr second attempt failed with RemoteSolrException HTTP {}", e.code(), e.getMessage());
+			logger.debug("Solr second attempt failed with RemoteSolrException HTTP {}: ", e.code(), e);
 		} catch (SolrServerException e) {
 			logger.warn("Solr second attempt failed with SolrServerException: ", e);
 			throw new RuntimeException("Solr exception during retry", e);
 		} catch (IOException e) {
+			// #1358 Likely never happens.
 			logger.warn("Solr second attempt failed with IOException: ", e);
 			throw new RuntimeException("Solr exception during retry", e);
 		}
